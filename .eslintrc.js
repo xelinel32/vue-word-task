4cb2982784a5a17b707f28ecfc0f2fda40d7646a
xelinel32 <artemsedlyar99@gmail.com>
2020-10-31 19:43:14 +0200

    change rules for netlify

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,8 @@ module.exports = {
   extends: ['plugin:vue/recommended', 'eslint:recommended', 'prettier/vue', 'plugin:prettier/recommended'],
   rules: {
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
